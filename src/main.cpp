fc0e13515f537dcc300b9591fb952f52c85ad392
cuongnguyenmanh839-byte <cuongnguyenmanh839@gmail.com>
2026-08-26 14:20:39 +0700

    update main.cpp threshold, loop

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,14 @@
 #include <Arduino.h>
 #define LED_PIN 2
 #define SENSOR_PIN A0
-int threshold = 600;
+int threshold = 700;
 
 void loop() {
     int value = analogRead(SENSOR_PIN);
-    if (value > threshold)
+    if (value > threshold) {
         digitalWrite(LED_PIN, HIGH);
-    else
+        delay(100);
+    } else {
         digitalWrite(LED_PIN, LOW);
+    }
 }
